aa02653cce388c8225527c200ce0d2fdf4129159
Nellow <prabeshpathak002@gmail.com>
2025-02-21 12:27:45 -0800

    add : Notfound and perms denied page

---
+++ app/permission-denied.tsx
@@ -0,0 +1,8 @@
+export default function PermissionDenied() {
+    return (
+      <div>
+        <h2>Permission Denied</h2>
+        <p>You do not have permission to access this page.</p>
+      </div>
+    );
+  }
